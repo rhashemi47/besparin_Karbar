b6ba62c9c95342f183563a15c81d1e257c4375ce
rhashemi47 <35556246+rhashemi47@users.noreply.github.com>
2019-01-07 18:57:04 +0330

    1397/10/18

--- app/src/main/java/com/besparina/it/karbar/PublicVariable.java
+++ app/src/main/java/com/besparina/it/karbar/PublicVariable.java
@@ -5,9 +5,10 @@ public class PublicVariable {
 	public static final String NAMESPACE = "http://tempuri.org/";
 	//public static final String URL = "http://192.168.3.5/WebERP/WebBesparin/WebService/BesparinUserWS.asmx";
 	//public static final String URL = "http://94.183.241.216:8070/WebERP/WebBesparin/WebService/BesparinUserWS.asmx";
-	public static final String URL = "http://besparina.com:8024/WebERP/WebBesparin/WebService/BesparinUserWS.asmx";
-	public static final String site = "http://besparina.com";
-	public static final String Recive_NumberPhone = "5000125475";
+	public static final String URL = "http://besparina.ir:8024/WebERP/WebBesparin/WebService/BesparinUserWS.asmx";
+	public static final String site = "http://besparina.ir";
+	public static final String Recive_NumberPhone1 = "5000125475";
+	public static final String Recive_NumberPhone2 = "100020400";
 	public static String LinkFileTextCheckVersion =  "http://besparina.ir:8024/AppUpdate/user/Version.txt";
 	public static String DownloadAppUpdateLinkAPK = "http://besparina.ir:8024/AppUpdate/user/app.apk";
 	public static boolean theard_GetPerFactor=true;
