81fc3ab35cb7fe10b255e93d0646dc3329505767
rhashemi47 <35556246+rhashemi47@users.noreply.github.com>
2019-01-18 00:34:14 +0330

    1397/10/28

--- app/src/main/java/com/besparina/it/karbar/FontMain.java
+++ app/src/main/java/com/besparina/it/karbar/FontMain.java
@@ -1,6 +1,9 @@
 package com.besparina.it.karbar;
 
 import android.app.Application;
+import android.content.Context;
+import android.support.multidex.MultiDex;
+import android.support.multidex.MultiDexApplication;
 
 import uk.co.chrisjenx.calligraphy.CalligraphyConfig;
 
@@ -8,7 +11,7 @@ import uk.co.chrisjenx.calligraphy.CalligraphyConfig;
  * Created by Rm on 03/04/2018.
  */
 
-public class FontMain extends Application {
+public class FontMain extends MultiDexApplication {
     @Override
     public void onCreate() {
         super.onCreate();
@@ -17,5 +20,10 @@ public class FontMain extends Application {
                 .setFontAttrId(R.attr.fontPath)
                 .build()
         );
+    }
+    @Override
+    protected void attachBaseContext(Context context) {
+        super.attachBaseContext(context);
+        MultiDex.install(this);
     }
 }
